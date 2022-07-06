1f41415714de75caf442c2539d075c9d1336bdc8
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2022-07-06 14:36:20 +0545

    component added and general css for the classes added on index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,26 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './App.css';
+
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import Services from './components/services/Services';
+import Footer from './components/footer/Footer';
+import Testimonial from './components/testimonials/Testimonial';
+import Portfolio from './components/portfolio/Portfolio';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='App'>
+      <Header />
+      <Nav />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonial />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
